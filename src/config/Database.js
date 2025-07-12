a992325ac743e9ddfc26a8487dcc19bf93fb5c53
jyotiranjanmohanty1997 <122288175+jyotiranjanmohanty1997@users.noreply.github.com>
2025-07-13 01:03:34 +0530

    get api

--- src/config/Database.js
+++ src/config/Database.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect("mongodb://localhost:27017/");
+  await mongoose.connect("mongodb://localhost:27017/nodeProject");
 };
 
 module.exports = connectDB;
-
